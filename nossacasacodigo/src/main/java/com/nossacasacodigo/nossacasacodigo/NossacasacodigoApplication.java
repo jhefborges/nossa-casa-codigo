f91a9cfd51cd5cc44f16b414ab785e26ba4f5eae
Jheferson Renan Borges <renanjheferson@gmail.com>
2019-11-10 21:40:05 -0300

    Commit inicial
    Adicionados arquivos iniciais, gerados por https://start.spring.io/

---
+++ nossacasacodigo/src/main/java/com/nossacasacodigo/nossacasacodigo/NossacasacodigoApplication.java
@@ -0,0 +1,13 @@
+package com.nossacasacodigo.nossacasacodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossacasacodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossacasacodigoApplication.class, args);
+	}
+
+}
---
+++ nossacasacodigo/src/test/java/com/nossacasacodigo/nossacasacodigo/NossacasacodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nossacasacodigo.nossacasacodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossacasacodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
